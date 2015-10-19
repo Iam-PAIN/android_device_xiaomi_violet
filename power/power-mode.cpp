41d0cce4f0469603eb33f67cc246178667d34e60
Steve Kondik <steve@cyngn.com>
2015-10-19 14:43:39 -0700

    violet: power: Add support for DT2W feature control
    Squashed into this commit:
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Dec 7 23:51:29 2019 +0100
    
        power: Follow symlinks when writing to dt2w toggle path
    
        * Some devices tend to symlink touchpanel related toggles
          to common path due to having to deal with different panels.
    
        Change-Id: I429e4c5acfbcd3ff26a019f919fbe880692160bd
    
    [ArianK16a]: Adapt to AIDL HAL
    [ghostrider-reborn]: Adapt to device power modes extension
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Md Abu Talha <abu.talha.asif.31@gmail.com>

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -16,7 +16,8 @@
 
 #include <aidl/android/hardware/power/BnPower.h>
 #include <android-base/file.h>
-#include <android-base/logging.h>
+#include "power-common.h"
+/*#include <android-base/logging.h>
 #include <linux/input.h>
 
 namespace {
@@ -53,6 +54,9 @@ int open_ts_input() {
     return fd;
 }
 }  // anonymous namespace
+*/
+
+#define DT2W_NODE "/sys/touchpanel/double_tap"
 
 namespace aidl {
 namespace android {
@@ -68,6 +72,7 @@ using ::aidl::android::hardware::power::Mode;
 bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
     switch (type) {
         case Mode::DOUBLE_TAP_TO_WAKE:
+        case Mode::LOW_POWER:
             *_aidl_return = true;
             return true;
         default:
@@ -77,7 +82,7 @@ bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
 
 bool setDeviceSpecificMode(Mode type, bool enabled) {
     switch (type) {
-        case Mode::DOUBLE_TAP_TO_WAKE: {
+        /*case Mode::DOUBLE_TAP_TO_WAKE: {
             int fd = open_ts_input();
             if (fd == -1) {
                 LOG(WARNING)
@@ -89,9 +94,14 @@ bool setDeviceSpecificMode(Mode type, bool enabled) {
             ev.code = SYN_CONFIG;
             ev.value = enabled ? kInputEventWakeupModeOn : kInputEventWakeupModeOff;
             write(fd, &ev, sizeof(ev));
-            close(fd);
+            close(fd);*/
+        case Mode::DOUBLE_TAP_TO_WAKE:
+            ::android::base::WriteStringToFile(enabled ? "1" : "0", DT2W_NODE, true);
             return true;
-        }
+        case Mode::LOW_POWER:
+            power_hint(POWER_HINT_LOW_POWER, reinterpret_cast<void*>(enabled));
+            return true;
+        /*}*/
         default:
             return false;
     }
