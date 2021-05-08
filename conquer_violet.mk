935d3b1261d5b2710b8a3a3c83112aedba650286
sackclothxashes <george140201@gmail.com>
2021-05-08 16:41:16 +0000

    violet: ConquerOS bringup
    Signed-off-by: sackclothxashes <george140201@gmail.com>

--- conquer_violet.mk
+++ conquer_violet.mk
@@ -21,20 +21,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common CygnusOS stuff.
-CYGNUS_BUILD_TYPE := OFFICIAL
+# Inherit some common ConquerOS stuff.
+$(call inherit-product, vendor/conquer/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GAPPS := true
-$(call inherit-product, vendor/cygnus/configs/common.mk)
+TARGET_GAPPS_ARCH := arm64
 
-PRODUCT_NAME := cygnus_violet
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := conquer_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V11.0.8.0.PFHINXM release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys" \
     PRODUCT_NAME="violet"
+    
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
